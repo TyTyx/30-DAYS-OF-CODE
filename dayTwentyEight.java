382d3db298864f3ef8f8006ebe6d5daf5356c807
Tylor <massmaileffect@gmail.com>
2018-04-14 21:38:10 +0000

    Day 28: RegEx, Patterns, and Intro to Databases

---
+++ dayTwentyEight.java
@@ -0,0 +1,30 @@
+// RegEx, Patterns, and Intro to Databases
+
+import java.io.*;
+import java.util.*;
+import java.util.Collections;
+import java.util.regex.*;
+
+public class dayTwentyEight {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        String emailRegEx = ".+@gmail\\.com$";
+        Pattern pattern = Pattern.compile(emailRegEx);
+        List<String> list = new ArrayList();
+        for (int i = 0; i < num; i++){
+            String name = scanner.next();
+            String email = scanner.next();
+            Matcher matcher = pattern.matcher(email);
+            if (matcher.find()){
+                list.add(name);
+            }
+        }
+        Collections.sort(list);
+        for (String name : list){
+            System.out.println(name);
+        }
+    }
+}
